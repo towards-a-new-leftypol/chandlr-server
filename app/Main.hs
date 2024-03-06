fc443e27bb6d18a669c2526803501ef1529b8e0e
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2024-03-05 20:45:07 -0500

    Work in progress - get front-end routes working server side
    - doesn't build
    - doesn't work
    - needs more of the front-end code in Common

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,28 @@
 module Main where
 
+import Miso (ToServerRoutes)
+import           Data.Proxy
+import qualified Network.Wai as Wai
+import qualified Network.Wai.Handler.Warp             as Wai
+import qualified Network.Wai.Middleware.RequestLogger as Wai
+import qualified Servant
+
+import qualified Common.FrontEnd.Routes as FE
+import qualified Common.FrontEnd.Action as FE
+
+newtype HtmlPage a = HtmlPage a
+  -- deriving (Show, Eq)
+
+type FrontEndRoutes = ToServerRoutes FE.Route HtmlPage FE.Action
+
+app :: Wai.Application
+app = undefined
+
+port :: Int
+port = 8888
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    putStrLn $ "Serving front-end on port " ++ show port
+
+    Wai.run port $ Wai.logStdout app
