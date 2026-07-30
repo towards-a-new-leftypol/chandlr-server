88ae06eb50602641284859e595de857acfd88476
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2026-07-30 15:27:45 -0400

    Update common, miso

--- src/Network/Client.hs
+++ src/Network/Client.hs
@@ -8,4 +8,4 @@ import Miso (Component, component, vfrag)
 import Common.Network.ClientTypes (Model (Uninitialized), Action)
 
 app :: Component parent props Model Action
-app = component Uninitialized (const $ return ()) (const $ const $ vfrag [])
+app = component Uninitialized (const $ return ()) (const $ const $ const $ vfrag [])
